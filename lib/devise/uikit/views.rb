b0845add0f3d1346351f48328e3e20a59a484705
eskey <esplants@outlook.com>
2016-02-22 20:02:15 +0900

    first commit

---
+++ lib/devise/uikit/views.rb
@@ -0,0 +1,9 @@
+require "devise/uikit/views/version"
+
+module Devise
+  module Uikit
+    module Views
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/devise/uikit/views/version.rb
@@ -0,0 +1,7 @@
+module Devise
+  module Uikit
+    module Views
+      VERSION = "0.1.0"
+    end
+  end
+end
